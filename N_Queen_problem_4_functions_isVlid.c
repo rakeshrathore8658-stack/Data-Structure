c9c347cad5e1eb740a8fa523c722e0feda81440d
Rakesh Rathore <240410077+rakeshrathore8658-stack@users.noreply.github.com>
2026-02-21 07:27:52 +0530

    Create N_Queen_problem_4_functions_isVlid.c

---
+++ N_Queen_problem_4_functions_isVlid.c
@@ -0,0 +1,11 @@
+//N Queen problem 
+// checking by 4 functions in isValid()  
+/*
+-> Columns wise Checking 
+-> row wise Checking 
+-> Checking on upper left diagonal
+-> Checking on upper right diagonal
+*/
+
+#include<stdio.h>
+#include<stdbool.h>
