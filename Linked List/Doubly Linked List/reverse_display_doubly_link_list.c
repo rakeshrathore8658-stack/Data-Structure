2f1fa6fe88fe5eb112f713c473e762f206537b81
Rakesh Rathore <240410077+rakeshrathore8658-stack@users.noreply.github.com>
2026-02-24 11:15:14 +0530

    Add reverse display function for doubly linked list

---
+++ Linked List/Doubly Linked List/reverse_display_doubly_link_list.c
@@ -0,0 +1,84 @@
+// reverse display Doubly Link list
+#include<stdio.h>
+#include<stdlib.h>
+struct Node *start = NULL;
+void insertAtB(int x);
+void insertAtE(int x);
+void showALL2();
+struct Node
+{
+  int data;
+  struct Node *prev;
+  struct Node *next;
+};
+
+void insertAtB(int x)
+{
+    struct Node *temp = malloc(sizeof(struct Node));
+    if(temp==NULL)
+    {
+        printf("No Space");
+        return;
+    }
+  temp -> data =x;
+  temp -> prev =NULL;
+  temp -> next =start;
+   
+  if(start != NULL)
+   {
+     start -> prev = temp;
+   }
+  start = temp;
+}
+
+
+void showALL2()  // reverse display function 
+{
+  if(start == NULL)
+  {
+    printf("List is Empty..");
+    return;
+  }
+  struct Node *temp=start; // treversing Node
+  while(temp -> next != NULL)
+    {
+      temp = temp -> next;
+    }
+
+  while(temp !=NULL)
+    {
+       printf("%d\n",temp -> data);
+      temp = temp -> prev;
+    }
+}
+
+void insertAtE(int x)
+{
+    if(start == NULL)
+    {
+        insertAtB(x);
+        return;
+    }
+     struct Node *temp = malloc(sizeof(struct Node));
+     temp -> data = x ;
+     temp -> next =NULL ;
+     
+     struct Node *temp1 = start; // traversing Node
+     
+     while(temp1->next != NULL)
+     {
+         temp1 = temp1 -> next;
+     }
+     temp1 -> next = temp;
+     temp -> prev  = temp1;
+}
+
+void main()
+{
+  insertAtE(10);
+  insertAtE(20);
+  insertAtE(30);
+  insertAtE(40);
+  insertAtE(50);
+  showALL2();
+}
