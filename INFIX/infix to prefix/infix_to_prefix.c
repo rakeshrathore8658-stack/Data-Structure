0a9886cb0b74304502ad8a45fa91fb6915c68e77
Rakesh Rathore <240410077+rakeshrathore8658-stack@users.noreply.github.com>
2026-03-04 20:10:55 +0530

    Implement infix to prefix conversion
    This program converts infix expressions to prefix notation using a stack-based approach.

---
+++ INFIX/infix to prefix/infix_to_prefix.c
@@ -0,0 +1,143 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+#define CAPACITY 50
+
+void push(char);
+char pop();
+char peek();
+bool isEmpty();
+int pr(char);
+void reverse(char x[], int n);
+
+char Stack[CAPACITY];
+int top = -1;
+
+void main()
+{
+    int i = 0;
+    int k = 0;
+
+    char input[CAPACITY];
+    char output[CAPACITY];
+
+    printf("Enter any Expression\n");
+    scanf("%s", input);
+
+    int n = strlen(input);
+
+    reverse(input, n);      // Step 1: reverse input
+
+    while(input[i] != '\0')
+    {
+        if(input[i] == '(')
+        {
+            push(input[i]);
+        }
+
+        else if(input[i] >= 65 && input[i] <= 90)   // A-Z
+        {
+            output[k++] = input[i];
+        }
+
+        else if(input[i] == ')')
+        {
+            while(peek() != '(')
+            {
+                output[k++] = pop();
+            }
+            pop();
+        }
+
+        else
+        {
+            while(!isEmpty() && pr(peek()) >= pr(input[i]))
+            {
+                output[k++] = pop();
+            }
+            push(input[i]);
+        }
+
+        i++;
+    }
+
+    while(!isEmpty())
+    {
+        output[k++] = pop();
+    }
+
+    output[k] = '\0';
+
+    reverse(output, k);     // Step 2: reverse output
+
+    printf("%s\n", output);
+}
+
+int pr(char ch)
+{
+    switch(ch)
+    {
+        case '(':
+            return 0;
+
+        case '+':
+        case '-':
+            return 1;
+
+        case '*':
+        case '/':
+            return 2;
+
+        case '^':
+            return 3;
+    }
+}
+
+void push(char x)
+{
+    Stack[++top] = x;
+}
+
+char pop()
+{
+    return Stack[top--];
+}
+
+char peek()
+{
+    return Stack[top];
+}
+
+bool isEmpty()
+{
+    if(top == -1)
+        return true;
+
+    return false;
+}
+
+void reverse(char x[], int n)
+{
+    int i=0, j=n-1;
+    char t;
+
+    while(i < j)
+    {
+        t = x[i];
+        x[i] = x[j];
+        x[j] = t;
+
+        if(x[i] == '(')
+            x[i] = ')';
+        else if(x[i] == ')')
+            x[i] = '(';
+
+        if(x[j] == '(')
+            x[j] = ')';
+        else if(x[j] == ')')
+            x[j] = '(';
+
+        i++;
+        j--;
+    }
+}
